269ad060e8fe45be13371b88b5cfff7c921b5807
LVALL <valeradracenko418@gmail.com>
2021-02-06 12:04:57 +0200

    Change patch modules

--- lib/patches/add_safety_attributes_patch.rb
+++ lib/patches/add_safety_attributes_patch.rb
@@ -1,15 +1,8 @@
 module ProjectRolesListing
   module RolePatch
     def self.included(base)
-      base.send(:include, InstanceMethods)
       base.class_eval do
         safe_attributes 'show_in_admin_projects'
-      end
-    end
-
-    module InstanceMethods
-      def allowed_to_view
-        self.where(show_in_admin_projects: true)
       end
     end
   end
